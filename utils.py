94a1f6a97c1e929095ec1692f0dd99f5991939c7
ndfla <88149941+ndfla@users.noreply.github.com>
2024-02-25 19:39:38 +0900

    move files and delete unnecessary files

--- utils.py
+++
@@ -1,9 +0,0 @@
-import time
-
-from flask import url_for
-
-
-# 元のurl_forと同じ引数
-def my_url_for(endpoint, **values):
-    url = url_for(endpoint, **values) 
-    return url + '?ts={}'.format(int(time.time()))
